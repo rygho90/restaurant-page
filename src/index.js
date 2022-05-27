d1c03ae4394572b9579ed02672af548a6ace4bf1
Ryan <rygho90@gmail.com>
2022-05-27 15:20:56 -0400

    Add dynamic header to JS file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-console.log("Beep Boop");
-
-console.log("Boop Beep")
-
 import './style.css';
+
+const content = document.querySelector(".content");
+
+const header = document.createElement("div");
+header.classList.add("header");
+
+const restaurantName = document.createElement("h1");
+restaurantName.classList.add("restaurant-name");
+restaurantName.textContent = "Ryan's Kitchen";
+
+const nav = document.createElement("nav");
+const homeButton = document.createElement("button");
+const menuButton = document.createElement("button");
+const contactButton = document.createElement("button");
+homeButton.textContent = "Home";
+menuButton.textContent = "Menu";
+contactButton.textContent = "Contact";
+homeButton.classList.add("button-nav");
+homeButton.classList.add("active");
+menuButton.classList.add("button-nav");
+contactButton.classList.add("button-nav");
+
+nav.append(homeButton);
+nav.append(menuButton);
+nav.append(contactButton);
+header.append(restaurantName);
+header.append(nav);
+content.append(header);
